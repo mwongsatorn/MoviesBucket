dea9e91e62299357f7ed6d380c237e0f976e27a4
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-03-02 16:56:00 +0700

    feat: route transition

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,17 @@ import AppFooter from "./components/AppFooter.vue";
   <div class="flex min-h-screen flex-col">
     <AppHeader></AppHeader>
     <RouterView v-slot="{ Component }">
-      <component :is="Component"></component>
+      <Transition
+        mode="out-in"
+        enter-active-class="transition duration-300"
+        leave-active-class="transition duration-300"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-1"
+        leave-from-class="opacity-1"
+        leave-to-class="opacity-0"
+      >
+        <component :is="Component"></component>
+      </Transition>
     </RouterView>
     <AppFooter></AppFooter>
   </div>
