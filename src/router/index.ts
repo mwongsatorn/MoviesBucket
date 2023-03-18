23bba1edbf21456d795d5472080869f655fcaefa
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-03-18 17:01:56 +0700

    feat: scroll to top when visit the  new route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,9 @@ import HomePage from "../views/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    return { top: 0 };
+  },
   routes: [
     {
       path: "/",
