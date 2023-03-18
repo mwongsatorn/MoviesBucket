f8f03147e7c03fcb0c2b69867442b1bb336acc92
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-03-18 16:00:59 +0700

    feat: suspense error handler

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
+import { ref, onErrorCaptured } from "vue";
+
+const error = ref();
+
+onErrorCaptured((e) => {
+  error.value = e;
+});
 </script>
 
 <template>
@@ -17,7 +24,19 @@ import AppFooter from "./components/AppFooter.vue";
         leave-from-class="opacity-1"
         leave-to-class="opacity-0"
       >
-        <component :is="Component"></component>
+        <Suspense timeout="0">
+          <template #default>
+            <component :is="Component"></component>
+          </template>
+          <template #fallback>
+            <div>
+              <div v-if="error">
+                {{ error }}
+              </div>
+              <div v-else>Loading......</div>
+            </div>
+          </template>
+        </Suspense>
       </Transition>
     </RouterView>
     <AppFooter></AppFooter>
