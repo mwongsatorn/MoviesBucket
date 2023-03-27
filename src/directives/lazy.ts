076262dc203f194b926a13278c8f9ab80ab18cb6
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-03-28 02:16:25 +0700

    refactor: v-lazy directive

--- src/directives/lazy.ts
+++ src/directives/lazy.ts
@@ -1,20 +1,29 @@
-export const vLazy = {
-  mounted: (el: HTMLImageElement) => {
-    const io = new IntersectionObserver(
-      (entries, observer) => {
-        const entry = entries[0];
-        if (entry.isIntersecting) {
-          const img = entry.target;
-          const src = img.getAttribute("data-src");
-          img.setAttribute("src", src!);
-          img.classList.toggle("opacity-0");
-          observer.disconnect();
-        }
-      },
-      {
-        threshold: 0.5,
-      }
-    );
+import type { Directive } from "vue";
+
+const io = new IntersectionObserver(handleIntersection, { threshold: 0.25 });
+
+function handleIntersection(
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      const img = entry.target;
+      const src = img.getAttribute("data-src");
+      img.removeAttribute("data-src");
+      img.setAttribute("src", src!);
+      img.classList.toggle("opacity-0");
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+export const vLazy: Directive<HTMLImageElement> = {
+  mounted: (el) => {
     io.observe(el);
+  },
+  unmounted: (el) => {
+    io.unobserve(el);
   },
 };
