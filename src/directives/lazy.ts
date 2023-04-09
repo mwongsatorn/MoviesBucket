a682f3c51027344f34f9340cf66565e882f92755
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-10 00:35:15 +0700

    feat: fade in after image finishes loading

--- src/directives/lazy.ts
+++ src/directives/lazy.ts
@@ -2,19 +2,24 @@ import type { Directive } from "vue";
 
 const io = new IntersectionObserver(handleIntersection, { threshold: 0.25 });
 
+function handleImageLoading(e: Event) {
+  if (e.target instanceof Element) {
+    e.target.classList.toggle("opacity-0");
+  }
+}
+
 function handleIntersection(
   entries: IntersectionObserverEntry[],
   observer: IntersectionObserver
 ) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log(entry);
-      const img = entry.target;
+      const img = entry.target as HTMLImageElement;
       const src = img.getAttribute("data-src");
       img.removeAttribute("data-src");
+      img.addEventListener("load", handleImageLoading);
       img.setAttribute("src", src!);
-      img.classList.toggle("opacity-0");
-      observer.unobserve(entry.target);
+      observer.unobserve(img);
     }
   });
 }
@@ -25,5 +30,6 @@ export const vLazy: Directive<HTMLImageElement> = {
   },
   unmounted: (el) => {
     io.unobserve(el);
+    el.removeEventListener("load", handleImageLoading);
   },
 };
