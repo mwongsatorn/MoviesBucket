71ec3634df77dc15ff12c728cc53a550ff485a86
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-10 00:31:42 +0700

    fix: redifine types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,127 +1,269 @@
-export type MediaType = "movie" | "tv";
-
-export interface PageResult {
+export interface PageResult<T> {
   page: number;
-  results: MediaDetails[];
+  results: T[];
   total_pages: number;
   total_results: number;
 }
 
-export interface GenreResult {
-  genres: Genre[];
-}
-
-export interface ErrorResult {
-  status_message: string;
-  status_code: number;
-}
-
-export interface MediaDetails {
+export interface ShortMovieDetails {
+  poster_path: string | null;
   adult: boolean;
-  backdrop_path: string;
+  overview: string;
+  release_date: string;
   genre_ids: number[];
   id: number;
-  original_language: string;
   original_title: string;
-  overview: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string | null;
   popularity: number;
-  poster_path: string;
-  release_date?: string;
-  first_air_date?: string;
-  title?: string;
-  name?: string;
+  vote_count: number;
   video: boolean;
   vote_average: number;
-  vote_count: number;
-  media_type?: MediaType;
-  // details
-  homepage?: string;
-  runtime?: number;
-  budget?: number;
-  revenue?: number;
-  status?: string;
-  tagline?: string;
-  genres?: Genre[];
-  production_companies?: any[];
-  videos?: {
-    results: Video[];
-  };
-  credits?: {
-    cast: Person[];
-    crew: Person[];
-  };
-  images?: {
+  media_type?: "movie";
+}
+
+export interface MovieDetails extends ShortMovieDetails {
+  belongs_to_collection: null | Record<string, string>;
+  budget: number;
+  genres: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+  homepage: string;
+  imdb_id: string | null;
+  production_companies: [
+    {
+      name: string;
+      id: number;
+      logo_path: string | null;
+      origin_country: string;
+    }
+  ];
+  production_countries: [
+    {
+      iso_3166_1: string;
+      name: string;
+    }
+  ];
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: [
+    {
+      iso_639_1: string;
+      name: string;
+    }
+  ];
+  status: string;
+  tagline: string | null;
+
+  //append_to_response
+
+  images: {
     backdrops: Image[];
     posters: Image[];
   };
-  recommendations?: {
-    page: number;
-    results: MediaDetails[];
+  vidoes: {
+    results: Video[];
   };
-  external_ids?: ExternalIds;
+  credits: {
+    cast: CreditsCastDetails[];
+    crew: CreditsCrewDetails[];
+  };
+  external_ids: SerieAndPersonExternalIds;
+  keywords: {
+    keywords: [
+      {
+        id: string;
+        name: string;
+      }
+    ];
+  };
+  recommendations: PageResult<ShortSerieDetails>;
 }
 
-export interface Genre {
+export interface ShortSerieDetails {
+  poster_path: string | null;
+  popularity: number;
   id: number;
+  backdrop_path: string;
+  vote_average: number;
+  overview: string;
+  first_air_date: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
   name: string;
+  original_name: string;
+  media_type?: "tv";
 }
 
-export interface ExternalIds {
-  imdb_id?: string;
-  facebook_id?: string;
-  instagram_id?: string;
-  twitter_id?: string;
-  linkedin_id?: string;
-  github_id?: string;
-  email?: string;
-  homepage?: string;
+export interface SerieDetails extends ShortSerieDetails {
+  created_by: [
+    {
+      id: number;
+      credit_id: string;
+      name: string;
+      gender: number;
+      profile_path: string | null;
+    }
+  ];
+  episode_run_time: number[];
+  genres: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+  homepage: string;
+  in_production: boolean;
+  last_air_date: string;
+  last_episode_to_air: {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path: string;
+    vote_average: number;
+    vote_count: number;
+  };
+  next_episode_to_air: null;
+  networks: [
+    {
+      name: string;
+      id: number;
+      logo_path: string | null;
+      origin_country: string;
+    }
+  ];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  production_companies: [
+    {
+      id: number;
+      logo_path: string;
+      name: string;
+      origin_country: string;
+    }
+  ];
+  production_countries: [
+    {
+      iso_3166_1: string;
+      name: string;
+    }
+  ];
+  seasons: [
+    {
+      air_date: string;
+      episode_count: number;
+      id: number;
+      name: string;
+      overview: string;
+      poster_path: string;
+      season_number: number;
+    }
+  ];
+  spoken_languages: [
+    {
+      english_name: string;
+      iso_639_1: string;
+      name: string;
+    }
+  ];
+  status: string;
+  tagline: string;
+  type: string;
+
+  //append_to_response
+
+  images: {
+    backdrops: Image[];
+    posters: Image[];
+  };
+  vidoes: {
+    results: Video[];
+  };
+  credits: {
+    cast: CreditsCastDetails[];
+    crew: CreditsCrewDetails[];
+  };
+  aggregate_credits: {
+    cast: AggregateCreditsCastDetails[];
+    crew: AggregateCreditsCrewDetails[];
+  };
+  external_ids: SerieAndPersonExternalIds;
+  keywords: {
+    results: [
+      {
+        id: string;
+        name: string;
+      }
+    ];
+  };
+  recommendations: PageResult<ShortSerieDetails>;
 }
 
-export interface Person {
+export interface ShortPersonDetails {
   adult: boolean;
-  gender: number;
+  gender: number | null;
   id: number;
   known_for_department: string;
   name: string;
   original_name: string;
-  profile_path: string;
   popularity: number;
-  cast_id?: number;
-  job?: string;
-  character?: string;
-  credit_id?: string;
+  profile_path: string | null;
+}
+
+export interface PersonDetails extends ShortPersonDetails {
+  birthday: string;
+  deathday: string | null;
+  also_known_as: string[];
+  biography: string;
+  place_of_birth: string | null;
+  imdb_id: string;
+  homepage: string;
+}
+
+export interface CreditsCastDetails extends ShortPersonDetails {
+  credit_id: string;
+  character: string;
   order: number;
-  //aggregate credits
-  roles?: [
+}
+
+export interface AggregateCreditsCastDetails extends ShortPersonDetails {
+  roles: [
     {
       credit_id: string;
-      character: string;
-      episode_count: number;
-    }
-  ];
-  jobs?: [
-    {
-      credit_id: string;
-      job: string;
+      characater: string;
       episode_count: number;
     }
   ];
   total_episode_count: number;
-  // details
-  also_known_as?: string[];
-  birthday?: string;
-  deathday?: string;
-  place_of_birth?: string;
-  homepage?: string;
-  biography?: string;
-  external_ids?: ExternalIds;
-  combined_credits?: {
-    cast?: MediaDetails[];
-    crew?: MediaDetails[];
-  };
-  images?: {
-    profiles: Image[];
-  };
+  order: number;
+}
+
+export interface AggregateCreditsCrewDetails extends ShortPersonDetails {
+  jobs: [
+    {
+      credit_id: string;
+      job: string;
+      episode_count: string;
+    }
+  ];
+  total_episode_count: number;
+  department: string;
+}
+
+export interface CreditsCrewDetails extends ShortPersonDetails {
+  credit_id: string;
+  job: string;
+  department: string;
 }
 
 export interface Video {
@@ -145,4 +287,21 @@ export interface Image {
   vote_average: number;
   vote_count: number;
   width: number;
+}
+
+export interface MovieExternalIds {
+  imdb_id: string | null;
+  facebook_id: string | null;
+  instagram_id: string | null;
+  twitter_id: string | null;
+}
+
+export interface SerieAndPersonExternalIds {
+  imdb_id: string | null;
+  facebook_id: string | null;
+  instagram_id: string | null;
+  twitter_id: string | null;
+  freebase_id: string | null;
+  freebase_mid: string | null;
+  tvrage_id: number | null;
 }
