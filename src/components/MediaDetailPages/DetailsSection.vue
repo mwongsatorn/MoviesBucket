c064b17ea26066dbc75ac55b2af7548e9622e917
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-11 15:31:33 +0700

    fix: different section tabs depending on the media type

--- src/components/MediaDetailPages/DetailsSection.vue
+++ src/components/MediaDetailPages/DetailsSection.vue
@@ -1,34 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import PersonCard from "@/components/PersonCard.vue";
+import { useRoute } from "vue-router";
 import CreditsSection from "@/components/MediaDetailPages/DetailsSection/CreditsSection.vue";
 import EpisodesSection from "@/components/MediaDetailPages/DetailsSection/EpisodesSection.vue";
 import ImagesSection from "@/components/MediaDetailPages/DetailsSection/ImagesSection.vue";
 import VideosSection from "@/components/MediaDetailPages/DetailsSection/VideosSection.vue";
-import type { CreditsCastDetails } from "@/types";
 
-interface Props {
-  cast: CreditsCastDetails[] | undefined;
-}
+const route = useRoute();
+const mediaType = route.name === "MovieDetails" ? "movie" : "serie";
 
 const activeTab = ref<string>("Credits");
 const sectionTabs: Record<string, any> = {
   Credits: CreditsSection,
-  Episodes: EpisodesSection,
+  ...(mediaType === "serie" && { Episodes: EpisodesSection }),
   Images: ImagesSection,
   Videos: VideosSection,
 };
-
-const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="flex flex-wrap place-items-center font-bold">
+  <div class="flex font-bold">
     <button
       v-for="(_, tab) in sectionTabs"
       :key="tab"
       @click="activeTab = tab"
-      class="w-1/3 px-4 py-4 text-center uppercase sm:w-1/4"
+      class="flex-grow px-2 py-4 text-center text-sm uppercase sm:text-base"
       :class="[activeTab === tab ? 'bg-rose-800 text-white' : '']"
     >
       {{ tab }}
