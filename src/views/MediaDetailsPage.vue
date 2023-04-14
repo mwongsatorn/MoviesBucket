db44bc837170a474c5f0068ba6fd035b46d60616
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-15 01:58:28 +0700

    refactor: use MediaOverview component in template

--- src/views/MediaDetailsPage.vue
+++ src/views/MediaDetailsPage.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { computed, provide } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute } from "vue-router";
 import { getMediaDetails } from "../composables/tmdb";
-
+import MediaOverview from "@/components/MediaOverview.vue";
 import MediaCard from "@/components/MediaCard.vue";
 import MediaDetails from "@/components/MediaDetails.vue";
 import MediaCarousel from "@/components/MediaCarousel.vue";
@@ -31,21 +31,19 @@ const mediaRuntime = computed(() => {
 });
 
 const mediaReleaseDate = computed(() => {
-  if (!mediaDetails.value) return null;
-  return "release_date" in mediaDetails.value
-    ? mediaDetails.value.release_date
-    : mediaDetails.value.first_air_date;
+  return "release_date" in mediaDetails.value!
+    ? mediaDetails.value!.release_date
+    : mediaDetails.value!.first_air_date;
 });
 
 const mediaReleaseYear = computed(() => {
-  return mediaReleaseDate.value?.split("-")[0];
+  return mediaReleaseDate.value!.split("-")[0];
 });
 
 const mediaTitle = computed(() => {
-  if (!mediaDetails.value) return null;
-  return "title" in mediaDetails.value
-    ? mediaDetails.value.title
-    : mediaDetails.value.name;
+  return "title" in mediaDetails.value!
+    ? mediaDetails.value!.title
+    : mediaDetails.value!.name;
 });
 
 const moneyFormat = new Intl.NumberFormat("en-us", {
@@ -77,6 +75,21 @@ const mediaCredits = computed(() => {
     : mediaDetails.value!.credits;
 });
 
+function mediaOverviewProps() {
+  return {
+    backdropPath: mediaDetails.value!.backdrop_path,
+    posterPath: mediaDetails.value!.poster_path,
+    title: mediaTitle.value,
+    releaseYear: mediaReleaseYear.value,
+    voteAverage: mediaDetails.value!.vote_average,
+    releaseDate: mediaReleaseDate.value,
+    runtime: mediaRuntime.value,
+    genres: mediaDetails.value!.genres,
+    tagline: mediaDetails.value!.tagline,
+    overview: mediaDetails.value!.overview,
+  };
+}
+
 function mediaCardProps(media: ShortMovieDetails | ShortSerieDetails) {
   return {
     id: media.id,
@@ -107,54 +120,7 @@ provide("images", mediaDetails.value?.images);
 
 <template>
   <main>
-    <section
-      id="overview"
-      class="relative bg-cover bg-center"
-      :style="{
-        backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${mediaDetails?.backdrop_path})`,
-      }"
-    >
-      <div class="absolute top-0 h-full w-full bg-black/80" />
-      <div
-        class="relative mx-auto flex max-w-7xl flex-col space-y-4 px-4 pb-12 pt-[calc(56px+2rem)] sm:flex-row sm:space-x-8"
-      >
-        <div class="shrink-0 self-center">
-          <img
-            class="w-[250px] border-2"
-            :src="`https://image.tmdb.org/t/p/w342/${mediaDetails?.poster_path}`"
-            alt=""
-          />
-        </div>
-        <div class="space-y-4 text-white">
-          <h1 class="text-2xl font-bold">
-            {{ mediaTitle }} ({{ mediaReleaseYear }})
-          </h1>
-          <div class="flex flex-wrap items-center gap-x-4 gap-y-4 text-sm">
-            <span>
-              {{ mediaDetails?.vote_average }}/10 | {{ mediaReleaseDate }} |
-              {{ mediaRuntime }}
-            </span>
-            <div class="flex flex-wrap items-center gap-x-4 gap-y-4">
-              <RouterLink
-                class="rounded-lg bg-rose-800 px-2 py-1 hover:bg-amber-500"
-                v-for="genre in mediaDetails?.genres"
-                :key="genre.id"
-                :to="`/genres/${genre.id}`"
-              >
-                {{ genre.name }}
-              </RouterLink>
-            </div>
-          </div>
-          <p class="italic text-white/50" v-if="mediaDetails?.tagline !== ''">
-            {{ mediaDetails?.tagline }}
-          </p>
-          <div class="space-y-4">
-            <h2 class="text-lg font-bold">Overview</h2>
-            <p>{{ mediaDetails?.overview }}</p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <MediaOverview v-bind="mediaOverviewProps()"></MediaOverview>
     <div class="mx-auto grid max-w-7xl md:grid-cols-[1fr_240px]">
       <div class="overflow-hidden">
         <MediaDetails />
