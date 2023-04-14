9a0475f1db42f7e139400f0932ad081847b8343a
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-14 21:20:54 +0700

    feat: MediaCarousel component

---
+++ src/components/MediaCarousel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  sectionName: string;
+  headerTitle: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <section :id="props.sectionName" class="mx-auto my-8 max-w-7xl px-4">
+    <h1 class="text-2xl font-bold">{{ props.headerTitle }}</h1>
+    <div class="main-scrollbar flex space-x-2 overflow-auto px-1 py-6">
+      <slot></slot>
+    </div>
+  </section>
+</template>
