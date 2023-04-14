b11982a2e914f300bc1cb7850d4b0a358857858e
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-04-15 02:30:56 +0700

    feat: transition for short cast and full credits section

--- src/components/MediaCredits.vue
+++ src/components/MediaCredits.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, inject } from "vue";
+import TransitionFade from "./TransitionFade.vue";
 import PersonCard from "@/components/PersonCard.vue";
 import PersonCreditItem from "@/components/PersonCreditItem.vue";
 import type {
@@ -42,57 +43,59 @@ function personCrewProps(
 </script>
 
 <template>
-  <section
-    v-show="activeSection === 'short-cast'"
-    id="short-cast"
-    class="my-8 px-4"
-  >
-    <div class="flex items-center gap-x-4">
-      <h1 class="text-2xl font-bold">Cast</h1>
-      <button
-        @click="activeSection = 'full-credits'"
-        class="font-bold text-rose-800"
-      >
-        View full credits
-      </button>
-    </div>
-    <div class="main-scrollbar flex space-x-2 overflow-auto px-1 py-6">
-      <PersonCard
-        v-for="person in credits!.cast.slice(0, 10)"
-        :key="person.id"
-        v-bind="personCastProps(person)"
-      />
-    </div>
-  </section>
-  <section
-    v-show="activeSection === 'full-credits'"
-    id="full-credits"
-    class="my-8 px-4"
-  >
-    <div class="flex items-center gap-x-4">
-      <h1 class="text-2xl font-bold">Cast ({{ credits?.cast.length }})</h1>
-      <button
-        @click="activeSection = 'short-cast'"
-        class="font-bold text-rose-800"
-      >
-        Back to the main cast
-      </button>
-    </div>
-    <div class="grid grid-cols-1 gap-x-2 gap-y-4 px-1 py-6 sm:grid-cols-2">
-      <PersonCreditItem
-        v-for="person in credits?.cast"
-        :key="person.id"
-        v-bind="personCastProps(person)"
-      />
-    </div>
+  <TransitionFade>
+    <section
+      v-if="activeSection === 'short-cast'"
+      id="short-cast"
+      class="my-8 px-4"
+    >
+      <div class="flex items-center gap-x-4">
+        <h1 class="text-2xl font-bold">Cast</h1>
+        <button
+          @click="activeSection = 'full-credits'"
+          class="font-bold text-rose-800"
+        >
+          View full credits
+        </button>
+      </div>
+      <div class="main-scrollbar flex space-x-2 overflow-auto px-1 py-6">
+        <PersonCard
+          v-for="person in credits!.cast.slice(0, 10)"
+          :key="person.id"
+          v-bind="personCastProps(person)"
+        />
+      </div>
+    </section>
+    <section
+      v-if="activeSection === 'full-credits'"
+      id="full-credits"
+      class="my-8 px-4"
+    >
+      <div class="flex items-center gap-x-4">
+        <h1 class="text-2xl font-bold">Cast ({{ credits?.cast.length }})</h1>
+        <button
+          @click="activeSection = 'short-cast'"
+          class="font-bold text-rose-800"
+        >
+          Back to the main cast
+        </button>
+      </div>
+      <div class="grid grid-cols-1 gap-x-2 gap-y-4 px-1 py-6 sm:grid-cols-2">
+        <PersonCreditItem
+          v-for="person in credits?.cast"
+          :key="person.id"
+          v-bind="personCastProps(person)"
+        />
+      </div>
 
-    <h1 class="text-2xl font-bold">Crew ({{ credits?.crew.length }})</h1>
-    <div class="grid grid-cols-1 gap-x-2 gap-y-4 px-1 py-6 sm:grid-cols-2">
-      <PersonCreditItem
-        v-for="person in credits?.crew"
-        :key="person.id"
-        v-bind="personCrewProps(person)"
-      />
-    </div>
-  </section>
+      <h1 class="text-2xl font-bold">Crew ({{ credits?.crew.length }})</h1>
+      <div class="grid grid-cols-1 gap-x-2 gap-y-4 px-1 py-6 sm:grid-cols-2">
+        <PersonCreditItem
+          v-for="person in credits?.crew"
+          :key="person.id"
+          v-bind="personCrewProps(person)"
+        />
+      </div>
+    </section>
+  </TransitionFade>
 </template>
