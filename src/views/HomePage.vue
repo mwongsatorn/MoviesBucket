01ae4437c0683baf889ddd18b44d8bb6bba84744
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-05-20 23:46:41 +0700

    refactor: state declaration and using props util

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,50 +1,19 @@
 <script setup lang="ts">
 import { getTrendingMedia } from "@/composables/tmdb";
-import type { ShortMovieDetails, ShortSerieDetails } from "@/types";
+import { mediaCardProps, mediaDisplayProps } from "@/utils/props";
 import MediaDisplay from "@/components/MediaDisplay.vue";
 import MediaCard from "@/components/MediaCard.vue";
 import MediaCarousel from "@/components/MediaCarousel.vue";
 
-const [trendingMediaResult, trendingMoviesResult, trendingSeriesResult] =
-  await Promise.all([
-    getTrendingMedia("all", "week"),
-    getTrendingMedia("movie", "week"),
-    getTrendingMedia("tv", "week"),
-  ]);
-
-const { data: trendingMedia } = trendingMediaResult;
-const { data: trendingMovies } = trendingMoviesResult;
-const { data: trendingSeries } = trendingSeriesResult;
-
-function mediaReleaseYear(date: string) {
-  return date.split("-", 1)[0];
-}
-
-function mediaCardProps(media: ShortMovieDetails | ShortSerieDetails) {
-  return {
-    id: media.id,
-    title: "title" in media ? media.title : media.name,
-    voteAverage: media.vote_average,
-    releaseDate:
-      "release_date" in media ? media.release_date : media.first_air_date,
-    posterPath: media.poster_path,
-    mediaType: "title" in media ? "movies" : "series",
-  };
-}
-
-function mediaDisplayProps(media: ShortMovieDetails | ShortSerieDetails) {
-  return {
-    id: media.id,
-    title: "title" in media ? media.title : media.name,
-    overview: media.overview,
-    releaseYear:
-      "release_date" in media
-        ? mediaReleaseYear(media.release_date)
-        : mediaReleaseYear(media.first_air_date),
-    backdropPath: media.backdrop_path,
-    mediaType: "title" in media ? "movies" : "series",
-  };
-}
+const [
+  { data: trendingMedia },
+  { data: trendingMovies },
+  { data: trendingSeries },
+] = await Promise.all([
+  getTrendingMedia("all", "week"),
+  getTrendingMedia("movie", "week"),
+  getTrendingMedia("tv", "week"),
+]);
 </script>
 
 <template>
