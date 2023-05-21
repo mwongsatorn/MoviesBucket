d073a68baf8caebbfa1ccf8569b5a1dcfe519910
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-05-21 13:17:47 +0700

    fix: use MediaCard component inside MediaCarousel component

--- src/components/MediaCarousel.vue
+++ src/components/MediaCarousel.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import type { ShortMovieDetails, ShortSerieDetails } from "@/types";
+import { mediaCardProps } from "@/utils/props";
+import MediaCard from "./MediaCard.vue";
+
 interface Props {
   sectionName: string;
   headerTitle: string;
+  media: ShortMovieDetails[] | ShortSerieDetails[];
 }
 
 const props = defineProps<Props>();
@@ -11,7 +16,11 @@ const props = defineProps<Props>();
   <section :id="props.sectionName" class="mx-auto my-8 max-w-7xl px-4">
     <h1 class="text-2xl font-bold">{{ props.headerTitle }}</h1>
     <div class="main-scrollbar flex space-x-2 overflow-auto px-1 py-6">
-      <slot></slot>
+      <MediaCard
+        v-for="item in media"
+        v-bind="mediaCardProps(item)"
+        :key="item.id"
+      />
     </div>
   </section>
 </template>
