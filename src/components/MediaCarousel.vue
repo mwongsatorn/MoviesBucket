6da8df2488ecf08e3b06365d739a629ff382f808
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-05-25 20:32:41 +0700

    feat: carousel controls

--- src/components/MediaCarousel.vue
+++ src/components/MediaCarousel.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import type { ShortMovieDetails, ShortSerieDetails } from "@/types";
 import { mediaCardProps } from "@/utils/props";
 import MediaCard from "./MediaCard.vue";
+import IconChevronLeft from "./Icons/IconChevronLeft.vue";
+import IconChevronRight from "./Icons/IconChevronRight.vue";
 
 interface Props {
   sectionName: string;
@@ -10,17 +13,50 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+
+const scroll = ref<HTMLElement | null>(null);
+
+function scrollHorizontally(dir: 1 | -1) {
+  const scrollElement = scroll.value;
+  if (scrollElement) {
+    const scrollWidth = scrollElement.clientWidth;
+    const scrollAmount = scrollWidth * dir;
+
+    scrollElement.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  }
+}
 </script>
 
 <template>
-  <section :id="props.sectionName" class="mx-auto my-8 max-w-7xl px-4">
+  <section :id="props.sectionName" class="relative mx-auto my-8 max-w-7xl px-4">
     <h1 class="text-2xl font-bold">{{ props.headerTitle }}</h1>
-    <div class="main-scrollbar flex space-x-2 overflow-auto px-1 py-6">
-      <MediaCard
-        v-for="item in media"
-        v-bind="mediaCardProps(item)"
-        :key="item.id"
-      />
+    <div class="relative @container/scroll">
+      <div
+        ref="scroll"
+        class="main-scrollbar flex snap-x snap-mandatory gap-x-2 overflow-x-auto scroll-smooth py-4"
+      >
+        <MediaCard
+          class="snap-start"
+          v-for="item in media"
+          v-bind="mediaCardProps(item)"
+          :key="item.id"
+        />
+      </div>
+      <button
+        @click="scrollHorizontally(-1)"
+        class="absolute left-0 top-4 flex h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-black text-white opacity-40 duration-300 hover:opacity-80 @md/scroll:w-[calc(10%-1.5rem)] @2xl/scroll:w-[calc(10%-2rem)] @5xl/scroll:w-[calc(10%-2.5rem)]"
+      >
+        <IconChevronLeft class="h-6 w-6" />
+      </button>
+      <button
+        @click="scrollHorizontally(1)"
+        class="absolute right-0 top-4 flex h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-black text-white opacity-40 duration-300 hover:opacity-80 @md/scroll:w-[calc(10%-1.5rem)] @2xl/scroll:w-[calc(10%-2rem)] @5xl/scroll:w-[calc(10%-2.5rem)]"
+      >
+        <IconChevronRight class="h-6 w-6" />
+      </button>
     </div>
   </section>
 </template>
