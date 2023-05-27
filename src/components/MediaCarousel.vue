c8a109af704102191a744b9f0a273522931784a3
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-05-27 21:50:38 +0700

    feat: show/hide controls depending on carousel's scroll position

--- src/components/MediaCarousel.vue
+++ src/components/MediaCarousel.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import type { ShortMovieDetails, ShortSerieDetails } from "@/types";
 import { mediaCardProps } from "@/utils/props";
 import MediaCard from "./MediaCard.vue";
@@ -12,22 +12,48 @@ interface Props {
   media: ShortMovieDetails[] | ShortSerieDetails[];
 }
 
+type Position = "START" | "END" | "NEITHER";
+
 const props = defineProps<Props>();
 
 const scroll = ref<HTMLElement | null>(null);
+const scrollPos = ref<Position>("START");
 
 function scrollHorizontally(dir: 1 | -1) {
   const scrollElement = scroll.value;
-  if (scrollElement) {
-    const scrollWidth = scrollElement.clientWidth;
-    const scrollAmount = scrollWidth * dir;
+  if (!scrollElement) return;
+  const scrollWidth = scrollElement.clientWidth;
+  const scrollAmount = scrollWidth * dir;
 
-    scrollElement.scrollBy({
-      left: scrollAmount,
-      behavior: "smooth",
-    });
-  }
+  scrollElement.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
 }
+
+function handleCarouselScroll() {
+  const scrollElement = scroll.value;
+  if (!scrollElement) return;
+  const isAtStart = scrollElement.scrollLeft === 0;
+  const isAtEnd =
+    scrollElement.scrollWidth -
+      scrollElement.scrollLeft -
+      scrollElement.clientWidth <
+    1;
+  const isBetweenStartAndEnd = !isAtStart && !isAtEnd;
+
+  if (isAtStart) scrollPos.value = "START";
+  if (isAtEnd) scrollPos.value = "END";
+  if (isBetweenStartAndEnd) scrollPos.value = "NEITHER";
+}
+
+onMounted(() => {
+  scroll.value?.addEventListener("scroll", handleCarouselScroll);
+});
+
+onUnmounted(() => {
+  scroll.value?.removeEventListener("scroll", handleCarouselScroll);
+});
 </script>
 
 <template>
@@ -47,13 +73,15 @@ function scrollHorizontally(dir: 1 | -1) {
       </div>
       <button
         @click="scrollHorizontally(-1)"
-        class="absolute left-0 top-4 flex h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-black text-white opacity-40 duration-300 hover:opacity-80 @md/scroll:w-[calc(10%-1.5rem)] @2xl/scroll:w-[calc(10%-2rem)] @5xl/scroll:w-[calc(10%-2.5rem)]"
+        class="absolute left-0 top-4 flex h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-black text-white opacity-40 duration-300 duration-300 hover:opacity-80 @md/scroll:w-[calc(10%-1.5rem)] @2xl/scroll:w-[calc(10%-2rem)] @5xl/scroll:w-[calc(10%-2.5rem)]"
+        :class="[scrollPos === 'START' ? 'hidden' : '']"
       >
         <IconChevronLeft class="h-6 w-6" />
       </button>
       <button
         @click="scrollHorizontally(1)"
         class="absolute right-0 top-4 flex h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-black text-white opacity-40 duration-300 hover:opacity-80 @md/scroll:w-[calc(10%-1.5rem)] @2xl/scroll:w-[calc(10%-2rem)] @5xl/scroll:w-[calc(10%-2.5rem)]"
+        :class="[scrollPos === 'END' ? 'hidden' : '']"
       >
         <IconChevronRight class="h-6 w-6" />
       </button>
