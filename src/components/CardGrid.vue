18d72577ed39ea007c7c47e150419b54fc203da3
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-07-01 21:22:07 +0700

    fix: use cards from parent instead of from props in CardGrid component

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -1,16 +1,6 @@
 <script lang="ts" setup>
 import { ref, onMounted, onUnmounted, onUpdated } from "vue";
-import MediaCard from "./MediaCard.vue";
-import { mediaCardProps } from "@/utils/props";
-import type { ShortMovieDetails, ShortSerieDetails } from "@/types";
 
-type Card = ShortMovieDetails | ShortSerieDetails;
-
-interface Props {
-  cards: Card[];
-  cardType: "media" | "search";
-  fetch: (page: number) => Promise<void>;
-}
 const io = new IntersectionObserver(handleGridScroll);
 
 function handleGridScroll(
@@ -19,37 +9,34 @@ function handleGridScroll(
 ) {
   const entry = entries[0];
   if (entry.isIntersecting) {
-    page = page + 1;
     observer.unobserve(entry.target);
-    props.fetch(page);
+    props.fetch && props.fetch();
   }
 }
-let page = 1;
 const cardsContainer = ref<HTMLElement | null>(null);
-const props = defineProps<Props>();
-await props.fetch(page);
+const props = defineProps<{
+  fetch?: () => Promise<void>;
+}>();
+
+props.fetch && (await props.fetch());
 
 onMounted(() => {
-  io.observe(cardsContainer.value!.lastElementChild!);
+  props.fetch && io.observe(cardsContainer.value!.lastElementChild!);
 });
 
 onUpdated(() => {
-  io.observe(cardsContainer.value!.lastElementChild!);
+  props.fetch && io.observe(cardsContainer.value!.lastElementChild!);
 });
 
 onUnmounted(() => {
-  io.disconnect();
+  props.fetch && io.disconnect();
 });
 </script>
 <template>
   <div
     ref="cardsContainer"
-    class="grid grid-cols-2 gap-1 py-4 @md:grid-cols-3 @2xl:grid-cols-4 @5xl:grid-cols-5"
+    class="grid grid-cols-2 gap-1.5 py-4 @md:grid-cols-3 @2xl:grid-cols-4 @5xl:grid-cols-5"
   >
-    <MediaCard
-      v-for="card in cards"
-      :key="card.id"
-      v-bind="mediaCardProps(card)"
-    />
+    <slot name="cards"></slot>
   </div>
 </template>
