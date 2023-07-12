13f056e491f17425eb4cf965d27df46e89f4e248
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-07-13 02:08:18 +0700

    fix: template structure in CardCarousel component

--- src/components/CardCarousel.vue
+++ src/components/CardCarousel.vue
@@ -65,45 +65,49 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <section :id="props.name" class="relative mx-auto my-8 max-w-7xl px-4">
-    <div class="flex flex-wrap items-center justify-between gap-x-4 gap-y-2">
-      <h1 class="text-xl font-bold capitalize sm:text-2xl">
-        {{ props.title }}
-      </h1>
-      <RouterLink
-        class="text-sm font-bold text-rose-800 hover:text-rose-900"
-        v-if="props.to"
-        :to="props.to"
-      >
-        View more
-      </RouterLink>
-      <slot name="button"></slot>
-    </div>
-    <div class="relative @container">
-      <div
-        ref="scroll"
-        class="main-scrollbar flex snap-x snap-mandatory gap-x-2 overflow-x-auto scroll-smooth py-4 childs:w-[45%] @md:childs:w-[30%] @2xl:childs:w-[22.5%] @5xl:childs:w-[18%]"
-      >
-        <slot name="cards"></slot>
+  <section :id="props.name" class="my-8 px-4">
+    <div class="relative mx-auto max-w-7xl">
+      <div class="flex flex-wrap items-center justify-between gap-x-4 gap-y-2">
+        <h1 class="text-xl font-bold capitalize sm:text-2xl">
+          {{ props.title }}
+        </h1>
+        <RouterLink
+          class="text-sm font-bold text-rose-800 hover:text-rose-900"
+          v-if="props.to"
+          :to="props.to"
+        >
+          View more
+        </RouterLink>
+        <slot name="button"></slot>
       </div>
-      <button
-        @click="scrollHorizontally(-1)"
-        class="absolute left-0 top-4 h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-gradient-to-r from-black to-transparent text-white opacity-40 duration-300 hover:opacity-80 @md:w-[calc(10%-1.5rem)] @2xl:w-[calc(10%-2rem)] @5xl:w-[calc(10%-2.5rem)]"
-        :class="[
-          scrollPos === 'START' || !isScrollable ? 'hidden' : 'hidden sm:flex',
-        ]"
-      >
-        <IconChevronLeft class="h-6 w-6" />
-      </button>
-      <button
-        @click="scrollHorizontally(1)"
-        class="absolute right-0 top-4 h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-gradient-to-l from-black to-transparent text-white opacity-40 duration-300 hover:opacity-80 @md:w-[calc(10%-1.5rem)] @2xl:w-[calc(10%-2rem)] @5xl:w-[calc(10%-2.5rem)]"
-        :class="[
-          scrollPos === 'END' || !isScrollable ? 'hidden' : 'hidden sm:flex',
-        ]"
-      >
-        <IconChevronRight class="h-6 w-6" />
-      </button>
+      <div class="relative @container">
+        <div
+          ref="scroll"
+          class="main-scrollbar flex snap-x snap-mandatory gap-x-2 overflow-x-auto scroll-smooth py-4 childs:w-[45%] @md:childs:w-[30%] @2xl:childs:w-[22.5%] @5xl:childs:w-[18%]"
+        >
+          <slot name="cards"></slot>
+        </div>
+        <button
+          @click="scrollHorizontally(-1)"
+          class="absolute left-0 top-4 h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-gradient-to-r from-black to-transparent text-white opacity-40 duration-300 hover:opacity-80 @md:w-[calc(10%-1.5rem)] @2xl:w-[calc(10%-2rem)] @5xl:w-[calc(10%-2.5rem)]"
+          :class="[
+            scrollPos === 'START' || !isScrollable
+              ? 'hidden'
+              : 'hidden sm:flex',
+          ]"
+        >
+          <IconChevronLeft class="h-6 w-6" />
+        </button>
+        <button
+          @click="scrollHorizontally(1)"
+          class="absolute right-0 top-4 h-[calc(100%-2.5rem)] w-[calc(10%-1rem)] items-center justify-center bg-gradient-to-l from-black to-transparent text-white opacity-40 duration-300 hover:opacity-80 @md:w-[calc(10%-1.5rem)] @2xl:w-[calc(10%-2rem)] @5xl:w-[calc(10%-2.5rem)]"
+          :class="[
+            scrollPos === 'END' || !isScrollable ? 'hidden' : 'hidden sm:flex',
+          ]"
+        >
+          <IconChevronRight class="h-6 w-6" />
+        </button>
+      </div>
     </div>
   </section>
 </template>
