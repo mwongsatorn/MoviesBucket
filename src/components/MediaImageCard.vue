5693e6fdf32fdaee70c2d250366ddb406a4a290b
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-07-23 01:13:31 +0700

    fix: define width of MediaImageCard component from parent instead of from itself

--- src/components/MediaImageCard.vue
+++ src/components/MediaImageCard.vue
@@ -1,31 +1,20 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { vLazy } from "@/directives/lazy";
 import IconExpand from "./Icons/IconExpand.vue";
 
 interface Props {
   filePath: string;
-  type: "backdrops" | "posters";
 }
 
 const props = defineProps<Props>();
-
-const itemClass = computed<string>(() => {
-  return props.type === "backdrops"
-    ? "w-full @sm:w-[calc(50%-8px)] @xl:w-[calc(33.33%-8px)] @4xl:w-[calc(25%-8px)]"
-    : "w-[calc(50%-8px)] @sm:w-[calc(33.33%-8px)] @xl:w-[calc(25%-8px)] @4xl:w-[calc(20%-8px)]";
-});
-const imageWidth = computed<string>(() => {
-  return props.type === "backdrops" ? "w780" : "w342";
-});
 </script>
 
 <template>
-  <div :class="[itemClass]" class="group relative cursor-pointer bg-gray-100">
+  <div class="group relative cursor-pointer bg-gray-100">
     <img
       v-lazy
-      class="h-full w-full object-cover object-center opacity-0 transition duration-500"
-      :data-src="`https://image.tmdb.org/t/p/${imageWidth}/${props.filePath}`"
+      class="h-full w-full opacity-0 transition duration-500"
+      :data-src="`https://image.tmdb.org/t/p/w300/${props.filePath}`"
       alt=""
     />
     <div
