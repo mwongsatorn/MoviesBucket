cbad1d29c62947d2f3a2d1a865f0686ac0831e39
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-07-23 01:58:53 +0700

    refactor: collaspe MediaDetails component to MediaDetails page

--- src/views/MediaDetailsPage.vue
+++ src/views/MediaDetailsPage.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import { computed, provide } from "vue";
+import { ref, computed, provide } from "vue";
 import { getMediaDetails } from "../composables/tmdb";
 import MediaOverview from "@/components/MediaOverview.vue";
-import MediaDetails from "@/components/MediaDetails.vue";
 import CardCarousel from "@/components/CardCarousel.vue";
 import MediaCard from "@/components/MediaCard.vue";
 import MediaMoreDetails from "@/components/MediaMoreDetails.vue";
+import MediaCredits from "@/components/MediaCredits.vue";
+import MediaVideos from "@/components/MediaVideos.vue";
+import MediaImages from "@/components/MediaImages.vue";
+import TransitionFade from "@/components/TransitionFade.vue";
 
 const props = defineProps<{
   media: "movies" | "series";
   id: string;
 }>();
+
+const activeTab = ref<string>("Credits");
+const sectionTabs: Record<string, any> = {
+  Credits: MediaCredits,
+  Videos: MediaVideos,
+  Images: MediaImages,
+};
 
 const type = props.media === "movies" ? "movie" : "tv";
 
@@ -36,7 +46,26 @@ provide("videos", mediaDetails.value?.videos.results);
     <MediaOverview :media="mediaDetails!" :type="props.media"></MediaOverview>
     <div class="mx-auto grid max-w-7xl md:grid-cols-[1fr_240px]">
       <div class="overflow-hidden">
-        <MediaDetails />
+        <div class="flex font-bold">
+          <button
+            v-for="(_, tab) in sectionTabs"
+            :key="tab"
+            @click="activeTab = tab"
+            class="flex-grow bg-gray-50 px-2 py-4 text-center text-sm uppercase sm:text-base"
+            :class="[
+              activeTab === tab
+                ? 'bg-rose-800 text-white'
+                : 'hover:bg-amber-500 hover:text-white',
+            ]"
+          >
+            {{ tab }}
+          </button>
+        </div>
+        <TransitionFade>
+          <KeepAlive>
+            <component :is="sectionTabs[activeTab]"></component>
+          </KeepAlive>
+        </TransitionFade>
         <CardCarousel name="recommendations" title="Recommendations">
           <template #cards>
             <MediaCard
